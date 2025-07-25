d48c5058f246cbc118698e2d3fd6e43f050ad506
sierrapablo <pablo@metrio.es>
2025-07-25 12:21:27 +0200

    ✨ feat: Introduce dynamic rotating text for hero section

---
+++ src/data/rotatingMessages.ts
@@ -0,0 +1,5 @@
+export const messages = [
+  "Durante años, hemos convertido simples inmuebles en hogares en los que ahora, nuestros clientes disfrutan de aquello que antes solo imaginaban.",
+  "Una buena reforma integral es un proceso que empieza por escuchar a nuestro cliente y presentarle soluciones a medida, asesorarle en la elección de materiales de primera calidad y brindarle un abanico de profesionales que cubrirán todas sus necesidades.",
+  "Cuando el último detalle está acabado, nuestro trabajo continúa estando a su disposición para cualquier pequeña reparación.",
+];
